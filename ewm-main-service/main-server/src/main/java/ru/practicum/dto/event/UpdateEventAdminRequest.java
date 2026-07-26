79c892962dde623f437e89227b27fd29c7d8385e
dudka-54 <kovboimolodoi@yandex.ru>
2026-07-27 00:58:01 +0400

    исправления

--- ewm-main-service/main-server/src/main/java/ru/practicum/dto/event/UpdateEventAdminRequest.java
+++ ewm-main-service/main-server/src/main/java/ru/practicum/dto/event/UpdateEventAdminRequest.java
@@ -1,7 +1,6 @@
 package ru.practicum.dto.event;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;

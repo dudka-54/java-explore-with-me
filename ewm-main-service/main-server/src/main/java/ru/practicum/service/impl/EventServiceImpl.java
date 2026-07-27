a9e3b8c1120f77a9882521712cfba228020f6f44
dudka-54 <kovboimolodoi@yandex.ru>
2026-07-27 16:02:09 +0400

    исправления

--- ewm-main-service/main-server/src/main/java/ru/practicum/service/impl/EventServiceImpl.java
+++ ewm-main-service/main-server/src/main/java/ru/practicum/service/impl/EventServiceImpl.java
@@ -9,7 +9,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.dto.event.*;
-import ru.practicum.dto.event.enumerated.AdminStateAction;
 import ru.practicum.dto.request.ParticipationRequestDto;
 import ru.practicum.exception.ConflictException;
 import ru.practicum.exception.NotFoundException;
